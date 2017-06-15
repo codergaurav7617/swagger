b5f93ad8051cda31a0aa8fc17c2c1a473b27f415
Otero Suarez, Orlando L. -ND <Orlando.L.Otero.Suarez.-ND@disney.com>
2017-06-15 16:31:13 -0400

    Changed - @Provider annotation is not needed

--- src/main/java/com/asimio/cxfdemo/rest/config/ProvidersConfig.java
+++ src/main/java/com/asimio/cxfdemo/rest/config/ProvidersConfig.java
@@ -1,13 +1,10 @@
 package com.asimio.cxfdemo.rest.config;
 
-import javax.ws.rs.ext.Provider;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
-@Provider
 @Configuration
 public class ProvidersConfig {
 
